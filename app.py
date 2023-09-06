089136d87434c8a5c4231dd5662fb93cf017c878
Mac <mgelvoleo@gmail.com>
2023-09-06 09:02:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask 
+from flask import Flask
+from socket import gethostname  # Import gethostname function
+
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def hello_world():
-    return 'Dockerized Flask App and the hostname was' + gethostname()
-
+    return 'Dockerized Flask App and the hostname was ' + gethostname()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
