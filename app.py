15786dcc388b1ee8845a8d4738fce5cd66c4b915
Mark <mgelvoleo@gmail.com>
2026-01-07 22:21:42 +0800

    test kuberbetes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD! testing 25"
+    return "Hello from Jenkins CI/CD! testing 10"
 
 
 if __name__ == "__main__":
