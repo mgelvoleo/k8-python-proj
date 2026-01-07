26262acabb06ee54fafda8e3b2e29d2e274020d1
Mark <mgelvoleo@gmail.com>
2026-01-07 22:11:07 +0800

    fix the pipeline Lint and test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD! testing 23"
+    return "Hello from Jenkins CI/CD! testing 24"
 
 
 if __name__ == "__main__":
